b207f74f761ee633ed7d51fd6144be87a9640980
“Aisling <aisling1odonovan@gmail.comgit commit -m Initial>
2018-11-25 23:54:19 +0000

    1st

--- settings/staging.py
+++ settings/staging.py
@@ -6,7 +6,10 @@ DEBUG = False
 
 # Load the ClearDB connection details from the environment variable
 DATABASES = {
-    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 SITE_URL = 'https://sheepsbridgekennel.herokuapp.com/'
