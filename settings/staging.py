426693d80a004a92b2bfc19cabd3d9446a58d8e7
“Aisling <aisling1odonovan@gmail.comgit commit -m Initial>
2018-11-25 23:27:23 +0000

    1st

---
+++ settings/staging.py
@@ -0,0 +1,30 @@
+from base import *
+import os
+
+
+DEBUG = False
+
+# Load the ClearDB connection details from the environment variable
+DATABASES = {
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+}
+
+SITE_URL = 'https://sheepsbridgekennel.herokuapp.com/'
+ALLOWED_HOSTS.append('sheepsbridgekennel.herokuapp.com')
+
+# Log DEBUG information to the console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
