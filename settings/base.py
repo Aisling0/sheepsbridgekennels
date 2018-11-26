fb02076a8bd428a6740f47ceee4f3992a09ae254
“Aisling <aisling1odonovan@gmail.comgit commit -m Initial>
2018-11-26 23:38:32 +0000

    SENDGRID_PASSWORD

--- settings/base.py
+++ settings/base.py
@@ -121,7 +121,7 @@ MEDIA_URL = '/media/'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ('SENDGRID_PASSWORD')
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
