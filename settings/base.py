b7105ba4ad3567709787585bba775a10c9eb0744
“Aisling <aisling1odonovan@gmail.comgit commit -m Initial>
2018-11-26 15:54:17 +0000

    1st

--- settings/base.py
+++ settings/base.py
@@ -120,9 +120,9 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sheepsbridgekennels@gmail.com'
+EMAIL_HOST_PASSWORD = '301N2014a!'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
